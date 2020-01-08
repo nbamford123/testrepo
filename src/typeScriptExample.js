129a07ee3eada4551af9adea11c24abf8a21e652
nbamford <bamfordn@b2b.regn.net>
2020-01-07 18:22:35 -0800

    husky rules, ok

--- src/typeScriptExample.js
+++
@@ -1,9 +0,0 @@
-function add(a, b) {
-    return a + b;
-}
-function getFullName(user) {
-    var _a = user.name, first = _a.first, middle = _a.middle, last = _a.last;
-    return [first, middle, last].filter(Boolean).join('');
-}
-add(1, "two");
-getFullName({ name: { first: 'Joe', midd1e: 'Bud', last: 'Matthews' } });
