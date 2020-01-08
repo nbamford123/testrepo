1280bbb5fc5f3d0aa1eae8d60612c5618b126a07
nbamford <bamfordn@b2b.regn.net>
2020-01-08 09:32:12 -0800

    husky test 2

--- src/typeScriptExample.ts
+++ src/typeScriptExample.ts
@@ -1,21 +1,21 @@
 function add(a: number, b: number): number {
-  return a + b
+  return a + b;
 }
 
 interface User {
   name: {
-    first: string
-    middle: string
-    last: string
-  }
+    first: string;
+    middle: string;
+    last: string;
+  };
 }
 function getFullName(user: User): string {
   const {
     name: {first, middle, last},
-  } = user
-  return [first, middle, last].filter(Boolean).join('')
+  } = user;
+  return [first, middle, last].filter(Boolean).join('');
 }
 
-add(1, 2)
+add(1, 2);
 
-getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}})
+getFullName({name: {first: 'Joe', middle: 'Bud', last: 'Matthews'}});
